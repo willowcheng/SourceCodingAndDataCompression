ff9a540fad5dfba4a10262c58910de86d8db7565
willowchan <e.wingus@gmail.com>
2014-09-25 18:10:34 -0400

    Add necessary comments

--- src/Assignment1/__exercise3__.py
+++ src/Assignment1/__exercise3__.py
@@ -28,9 +28,11 @@ class Tag_Encode(object):
     fx = []
     # variable temp will be used in temporary storage
     temp = 0
+    # output is the encode sequence, which is initialized as blank list
     output = []
 
     def __init__(self, model, sequence):
+        '''Initialize letters and probabilities with model (comprise of such two lists)'''
         self.letters = model[0]
         self.probabilities = model[1]
         self.sequence = sequence
@@ -42,31 +44,39 @@ class Tag_Encode(object):
         for i in self.probabilities:
             temp = self.temp
             self.temp += i
+            # get a list with 3 items, each item is made of lower Fx and upper Fx
             self.fx.append([temp, self.temp])
         
     def encode_sequence_generate(self, sequence):
+        '''Generate encode sequence with bounds updating and scale methods'''
         for i in sequence:
             bounds = self.fx[self.letters.index(i)]
+            # decide lower Fx and upper Fx which depends on the letter
             lower_fx, upper_fx = bounds[0], bounds[1]
             self.update_bounds(lower_fx, upper_fx, self.lower_bound, self.upper_bound)
             while (self.upper_bound < 0.5 or self.lower_bound >= 0.5):
+                # in such conditions, choice proper scale and add 0 or 1 to output
                 if self.upper_bound < 0.5:
                     self.e1_scale()
                     self.output.append("0")
                 elif self.lower_bound >= 0.5:
                     self.e2_scale()
                     self.output.append("1")
+        # add 100000 as the end of output
         self.output.append("100000")
             
     def update_bounds(self, lower_fx, upper_fx, lower_bound, upper_bound):
+        '''Update bounds as soon as a new letter comes'''
         self.lower_bound = lower_bound + (upper_bound - lower_bound) * lower_fx
         self.upper_bound = lower_bound + (upper_bound - lower_bound) * upper_fx
         
     def e1_scale(self):
+        '''Realize E1 scale method'''
         self.lower_bound *= 2
         self.upper_bound *= 2
         
     def e2_scale(self):
+        '''Realize E2 scale method'''
         self.lower_bound = 2 * (self.lower_bound - 0.5)
         self.upper_bound = 2 * (self.upper_bound - 0.5)
         
@@ -74,6 +84,7 @@ class Tag_Encode(object):
         return self.output
 
     def clear(self):
+        '''Clear all variables in Class'''   
         self.fx = []
         self.temp = 0
         self.output = []
@@ -82,10 +93,10 @@ class Tag_Encode(object):
         self.model = {}
         self.sequence = []
         
-my_encode_sequence = Tag_Encode(MODEL, SEQUENCE_1).get_output() 
-print("Result: ") 
-print(my_encode_sequence) 
-print("Encoded sequence: ")
+# instantiate with SEQUENCE_1
+my_encode_sequence = Tag_Encode(MODEL, SEQUENCE_1).get_output()
+# print sequence list as well as encoded sequence 
+print("Result: ", my_encode_sequence) 
+print("Encoded sequence: ", end="")
 for i in my_encode_sequence:
     print(i, end="")
-my_encode_sequence.clear()
