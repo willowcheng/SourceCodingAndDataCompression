242bb75a628d9746025dda249cb2634bd85e0eaa
willowchan <e.wingus@gmail.com>
2014-09-25 17:58:05 -0400

    Finished encoded part, version 1.2

--- src/Assignment1/__exercise3__.py
+++ src/Assignment1/__exercise3__.py
@@ -10,14 +10,16 @@ needed, make necessary assumptions.
 Created on Sep 25, 2014
 
 @author: Liu Cheng
-@version: 1.0
+@version: 1.2
 '''
 
-MODEL = {1: 0.8, 2 :0.02, 3: 0.18}
-SEQUENCE_1 = [1, 3, 2, 1]
-SEQUENCE_2 = [3, 2, 1, 1]
+LETTERS = ["1", "2" , "3"]
+PROBABILITIES = [0.8, 0.02, 0.18]
+MODEL = [LETTERS, PROBABILITIES]
+SEQUENCE_1 = ["1", "3", "2", "1"]
+SEQUENCE_2 = ["3", "2", "1", "1"]
 
-class Tag_Generation(object):
+class Tag_Encode(object):
     '''Generation of arithmetic encode for given sequence'''
     # initialize lower bound and upper bound with 0 and 1
     lower_bound = 0
@@ -29,40 +31,61 @@ class Tag_Generation(object):
     output = []
 
     def __init__(self, model, sequence):
-        self.model = model
+        self.letters = model[0]
+        self.probabilities = model[1]
         self.sequence = sequence
-        self.fx_generation(model)
-        self.encode_sequence_generate(sequence)
+        self.fx_generation()
+        self.encode_sequence_generate(self.sequence)
         
-    def fx_generation(self, model):
+    def fx_generation(self):
         '''Calculate the Fx(1), Fx(2), Fx(3), etc. Store these values in fx[]'''
-        for i in list(self.model.values()):
+        for i in self.probabilities:
             temp = self.temp
             self.temp += i
             self.fx.append([temp, self.temp])
-            print(self.fx)
-        self.temp = 0
         
     def encode_sequence_generate(self, sequence):
         for i in sequence:
-            bounds = self.fx[list(self.model.keys()).index(i)]
+            bounds = self.fx[self.letters.index(i)]
             lower_fx, upper_fx = bounds[0], bounds[1]
-            self.update_bounds(lower_fx, upper_fx)
-            if self.upper_bound < 0.5:
-                self.e1_scale()
-                self.output.append("0")
-            elif self. lower_bound >= 0.5:
-                self.e2_scale()
-                self.output.append("1")
+            self.update_bounds(lower_fx, upper_fx, self.lower_bound, self.upper_bound)
+            while (self.upper_bound < 0.5 or self.lower_bound >= 0.5):
+                if self.upper_bound < 0.5:
+                    self.e1_scale()
+                    self.output.append("0")
+                elif self.lower_bound >= 0.5:
+                    self.e2_scale()
+                    self.output.append("1")
         self.output.append("100000")
             
+    def update_bounds(self, lower_fx, upper_fx, lower_bound, upper_bound):
+        self.lower_bound = lower_bound + (upper_bound - lower_bound) * lower_fx
+        self.upper_bound = lower_bound + (upper_bound - lower_bound) * upper_fx
         
-    def update_bounds(self, lower_fx, upper_fx):
-        self.lower_bound = self.lower_bound + (self.upper_bound - self.lower_bound) * lower_fx
-        self.upper_bound = self.lower_bound + (self.upper_bound - self.lower_bound) * upper_fx
+    def e1_scale(self):
+        self.lower_bound *= 2
+        self.upper_bound *= 2
+        
+    def e2_scale(self):
+        self.lower_bound = 2 * (self.lower_bound - 0.5)
+        self.upper_bound = 2 * (self.upper_bound - 0.5)
         
     def get_output(self):
         return self.output
+
+    def clear(self):
+        self.fx = []
+        self.temp = 0
+        self.output = []
+        self.lower_bound = 0
+        self.upper_bound = 1
+        self.model = {}
+        self.sequence = []
         
-my_encode_sequence = Tag_Generation(MODEL, SEQUENCE_1)   
-print(my_encode_sequence.get_output)
+my_encode_sequence = Tag_Encode(MODEL, SEQUENCE_1).get_output() 
+print("Result: ") 
+print(my_encode_sequence) 
+print("Encoded sequence: ")
+for i in my_encode_sequence:
+    print(i, end="")
+my_encode_sequence.clear()
